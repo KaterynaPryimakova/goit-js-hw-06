5dc5fccdba9a83e2c14a330f3cd2dbe46f59b46f
KaterynaPryimakova <134732205+KaterynaPryimakova@users.noreply.github.com>
2023-10-05 14:28:58 +0300

    Update task-07.js

--- js/task-07.js
+++ js/task-07.js
@@ -1,7 +1,6 @@
 const range = document.querySelector("#font-size-control");
 const text = document.querySelector("#text");
-const callback = (event) => {
-    return (text.style.fontSize = event.currentTarget.getAttribute("max"));
-};
-
 range.addEventListener("input", callback);
+function callback(event) {
+    text.style.fontSize = event.currentTarget.value + "px";
+}
