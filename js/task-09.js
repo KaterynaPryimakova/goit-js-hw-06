f887316fccbd520e7251755110eae63d8d1e455e
KaterynaPryimakova <134732205+KaterynaPryimakova@users.noreply.github.com>
2023-10-05 21:52:14 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,13 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
+    return `#${Math.floor(Math.random() * 16777215)
+        .toString(16)
+        .padStart(6, 0)}`;
+}
+const background = document.querySelector("body");
+const changeButton = document.querySelector(".change-color");
+const result = document.querySelector(".color");
+changeButton.addEventListener("click", changeColor);
+function changeColor() {
+    background.style.backgroundColor = getRandomHexColor();
+    result.textContent = background.style.backgroundColor;
 }
