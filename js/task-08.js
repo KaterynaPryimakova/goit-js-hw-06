bdd2e2e0fee8723d810d571566d4823fec2232f1
KaterynaPryimakova <134732205+KaterynaPryimakova@users.noreply.github.com>
2023-10-05 15:24:15 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,19 @@
-
+const loginForm = document.querySelector(".login-form");
+loginForm.addEventListener("submit", callback);
+function callback(event) {
+    event.preventDefault();
+    const form = event.target.elements;
+    const userEmail = form.email.value;
+    const userPassWord = form.password.value;
+    if (userEmail === "" || userPassWord === "") {
+        alert(`Попередження! Всі поля повинні бути заповнені!`);
+    }
+    const userInfo = {
+        email: userEmail,
+        password: userPassWord,
+    };
+    if (userEmail !== "" || userPassWord !== "") {
+        console.log(userInfo);
+    }
+    event.target.reset();
+}
